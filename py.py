97ac0d7f2658b2b93d0e6bf4ae9b5fbbae0202d8
emilViuf <emilviuf@gmail.com>
2022-08-28 23:33:03 +0200

    Added Marker and tiles

--- py.py
+++ py.py
@@ -1,5 +1,7 @@
 import folium
-map = folium.Map(location=[55.65310221424613, 12.142294332895851], zoom_start=16)
+map = folium.Map(location=[55.65310221424613, 12.142294332895851], zoom_start=12, tiles="Stamen Terrain")
+
+map.add_child(folium.Marker(location=[55.65310221424613, 12.142294332895851], popup="This is RUC", icon=folium.Icon(color='green')))
 #do -> dir(folium) ... to get a list of folium directory
 #do -> help(folium.map) ... to see what you can pass to it
 # folium makes a map by converting python into js & html/css.
