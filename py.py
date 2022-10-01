a459170d06674ecb622e5ea7ff4dbdae50a5094e
emilViuf <emilviuf@gmail.com>
2022-10-01 23:38:12 +0200

    Changed popups to be featuregroups instead

--- py.py
+++ py.py
@@ -1,26 +1,30 @@
 import folium
 map = folium.Map(location=[55.65310221424613, 12.142294332895851], zoom_start=10, tiles="Stamen Terrain")
-
-fg = folium.FeatureGroup(name="Locations")
-fg.add_children(folium.Marker(location=[55.65310221424613, 12.142294332895851], popup="This is RUC", icon=folium.Icon(color='green')))
-fg.add_child(folium.Marker(location=[55.68376107342263, 12.514369497178908], popup="This is CBS", icon=folium.Icon(color='blue')))
-map.add_child(fg)
-
-
-
-
 mapwToner = folium.Map(location=[55.65310221424613, 12.142294332895851], zoom_start=10, tiles="Stamen toner")
-mapwToner.add_child(folium.Marker(location=[55.65310221424613, 12.142294332895851], popup="This is RUC", icon=folium.Icon(color='green')))
-mapwToner.add_child(folium.Marker(location=[55.68376107342263, 12.514369497178908], popup="This is CBS", icon=folium.Icon(color='blue')))
-
 mapwColor = folium.Map(location=[55.65310221424613, 12.142294332895851], zoom_start=10, tiles="Stamen Water Color")
-mapwColor.add_child(folium.Marker(location=[55.65310221424613, 12.142294332895851], popup="This is RUC", icon=folium.Icon(color='green')))
-mapwColor.add_child(folium.Marker(location=[55.68376107342263, 12.514369497178908], popup="This is CBS", icon=folium.Icon(color='blue')))
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[55.65310221424613, 12.142294332895851], popup="This is RUC", icon=folium.Icon(color='green')))
+fg.add_child(folium.Marker(location=[55.68376107342263, 12.514369497178908], popup="This is CBS", icon=folium.Icon(color='blue')))
+
+fgT = folium.FeatureGroup(name="My Map")
+fgT.add_child(folium.Marker(location=[55.65310221424613, 12.142294332895851], popup="This is RUC", icon=folium.Icon(color='green')))
+fgT.add_child(folium.Marker(location=[55.68376107342263, 12.514369497178908], popup="This is CBS", icon=folium.Icon(color='blue')))
+
+fgC = folium.FeatureGroup(name="My Map")
+fgC.add_child(folium.Marker(location=[55.65310221424613, 12.142294332895851], popup="This is RUC", icon=folium.Icon(color='green')))
+fgC.add_child(folium.Marker(location=[55.68376107342263, 12.514369497178908], popup="This is CBS", icon=folium.Icon(color='blue')))
+
+
+map.add_child(fg)
+mapwToner.add_child(fgT)
+mapwColor.add_child(fgC)
+
+
 
 #do -> help(folium.map) ... to see what you can pass to it
 # folium makes a map by converting python into js & html/css.
 map.save("Map1.html")
 mapwToner.save("Map2.html")
 mapwColor.save("Map3.html")
-
 # ^ This creates the html file
